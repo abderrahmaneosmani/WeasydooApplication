cc166956e855a2c59c3433b94a1aae3de7f0f3b4
Osmani Abderrahmane <abderrahmaneosmani2@gmail.com>
2024-05-18 11:55:54 +0100

    setup navigation

--- App.tsx
+++ App.tsx
@@ -6,35 +6,14 @@
  */
 
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  Text,
-  useColorScheme,
-} from 'react-native';
-
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {NavigationContainer} from '@react-navigation/native';
+import {BottomNavigation} from './navigation/BottomNavigation';
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Text>Hello </Text>
-      </ScrollView>
-    </SafeAreaView>
+    <NavigationContainer>
+      <BottomNavigation />
+    </NavigationContainer>
   );
 }
 
